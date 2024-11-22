b098f5c3dfe8c64e838dcd834dac28ae27ea1ad7
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-11-22 12:02:07 -0400

    todo app

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
+	todos.print()
+
+	storage.Save(todos)
+}
